af2145815a055d5c92b567c112b78bcde649133d
officialp123 <96476199+officialp123@users.noreply.github.com>
2022-10-29 19:43:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+(function() {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+
+    let tl = gsap.timeline();
+
+    tl.from('.navitem', {
+            stagger: .3,
+            duration: 2,
+            y: 20,
+            delay: 7,
+            ease: 'Expo.easeInOut',
+            opacity: 0
+        })
+        .from('#smline', {
+            width: 0,
+            duration: 1,
+            ease: 'Expo.easeInOut',
+        }, '-=2')
+        .from('.leftitem', {
+            stagger: .3,
+            duration: 2,
+            y: 20,
+            ease: 'Expo.easeInOut',
+            opacity: 0
+        }, '-=2')
+        .from('#right img', {
+            duration: 2,
+            scale: 1.05,
+            ease: 'Expo.easeInOut',
+            opacity: 0
+        }, '-=2')
+})();
